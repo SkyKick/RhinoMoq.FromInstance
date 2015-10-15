fae8a802dfb402ed79bfd7e427730bc45f008075
Philip Pittle <philip.pittle@skykick.com>
2015-10-14 18:49:25 -0700

    Added initial extension method stubs

---
+++ Moq.FromInstance/FromInstanceExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Moq.Language;
+
+namespace Moq.FromInstance
+{
+    public static class FromInstanceExtension
+    {
+        /// <summary>
+        /// <see cref="Mock{T}.Setup"/>s all members in type <typeparamref name="T"/> on
+        /// <paramref name="mock"/> so that they <see cref="IReturns{TMock,TResult}.Returns(TResult)"/>
+        /// against <paramref name="instance"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// new Mock<IFoo>()
+        ///     .SetupFromInstance(new Foo()); 
+        ///  ]]>
+        /// </code>
+        /// </example>
+        /// <returns>
+        /// Returns <paramref name="mock"/>.
+        /// </returns>
+        public static Mock<T> SetupFromInstance<T>(this Mock<T> mock, T instance)
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RhinoMocks.FromInstance/FromInstanceExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Rhino.Mocks;
+using Rhino.Mocks.Interfaces;
+
+namespace RhinoMocks.FromInstance
+{
+    public static class FromInstanceExtension
+    {
+        /// <summary>
+        /// <see cref="RhinoMocksExtensions.Stub{T}"/>s all members in type <typeparamref name="T"/> on
+        /// <paramref name="mock"/> so that they <see cref="IMethodOptions{T}.Do"/>
+        /// against <paramref name="instance"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// MockRepository
+        ///     .GenerateMock<IFoo>()
+        ///     .StubFromInstance(new Foo()); 
+        ///  ]]>
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// It is necessary to add new <see cref="RhinoMocksExtensions.Stub{T}"/>s BEFORE
+        /// calling <see cref="StubFromInstance{T}"/>.  Calling <see cref="RhinoMocksExtensions.Stub{T}"/>
+        /// after, will be ignored.
+        /// </remarks>
+        /// <returns>
+        /// Returns <paramref name="mock"/>.
+        /// </returns>
+        public static T StubFromInstance<T>(this T mock, T instance)
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
